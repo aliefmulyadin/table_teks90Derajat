617881138e33f00ec8b3f87e48801002c52933c7
M Alief Mulyadin <67978192+aliefmulyadin@users.noreply.github.com>
2023-10-09 12:48:17 +0700

    Update cobacoba.php

--- cobacoba.php
+++ cobacoba.php
@@ -11,10 +11,8 @@
         }
 
         .responsive-table {
-            width: auto;
+            width: 100%;
             border-collapse: collapse;
-            display: grid;
-            grid-auto-flow: column;
         }
 
         .responsive-table th,
@@ -23,6 +21,7 @@
             padding: 8px;
             text-align: left;
             overflow: hidden;
+            white-space: nowrap;
         }
 
         .rotated-text {
@@ -35,34 +34,60 @@
             width: auto;
             /* Mengikuti lebar teks yang diputar */
         }
+
+
+        @media (max-width: 768px) {
+
+            .responsive-table th,
+            .responsive-table td {
+                font-size: 14px;
+            }
+
+            .rotated-text {
+                display: none;
+            }
+        }
+
+        @media (max-width: 480px) {
+
+            .responsive-table th,
+            .responsive-table td {
+                font-size: 12px;
+            }
+        }
     </style>
+
 </head>
 
 <body>
     <div class="table-container">
         <table class="responsive-table">
             <tr>
+                <th rowspan="2">
+                    <div>TOTAL PIUTANG</div>
+                    <div>Rp. 57.824.570.772</div>
+                </th>
+                <th rowspan="2" class="rotated-text">PIUTANG</th>
+                <th class="rotated-text">UNBILL</th>
                 <th>Kolom 1</th>
                 <th>Kolom 2</th>
                 <th>Kolom 3</th>
                 <th>Kolom 4</th>
                 <th>Kolom 5</th>
                 <th>Kolom 6</th>
-                <th class="rotated-text">Kolom 7</th>
             </tr>
             <tr>
+                <td class="rotated-text">BILLED</td>
                 <td>Data 1</td>
                 <td>Data 2</td>
                 <td>Data 3</td>
                 <td>Data 4</td>
                 <td>Data 5</td>
                 <td>Data 6</td>
-                <td class="rotated-text">Data 7</td>
             </tr>
             <!-- Tambahkan baris data lainnya sesuai kebutuhan -->
         </table>
     </div>
-
 </body>
 
 </html>
